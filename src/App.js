ec7692bd83c63213341cb3a6a4b746b2574ef9fb
Mohammd Mustafa <68998715+MoMust@users.noreply.github.com>
2024-01-16 22:58:51 +0100

    issue with projects page, no animation update

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import Home from "./Components/Home/Home.jsx";
 import About from "./Components/About/About.jsx";
 import MyProjects from "./Components/MyProjects/MyProjects.jsx";
 import Contact from "./Components/Contact/Contact.jsx";
-import Nav from './Components/Nav/Nav.js';
+// import Nav from './Components/Nav/Nav.js';
 import NavV2 from './Components/NavV2/Nav.jsx';
 import './App.css'
+
+
 function App() {
   return (
     <>
