11becfb9f4e0742beefc66f2b532dbc91dfb3f50
Mohammed Mustafa <68998715+MoMust@users.noreply.github.com>
2024-08-08 21:12:51 +0200

    For v3, added new animation dropdown for skills (#15)

--- src/Components/Home/Skills/Skills.jsx
+++ src/Components/Home/Skills/Skills.jsx
@@ -1,25 +1,26 @@
-import React from 'react'
-import './Skills.css'
+import React from "react";
+import "./Skills.css";
 const Skills = (props) => {
   return (
     <div className="category col-4 text-center">
       <div className="d-flex flex-column text-center">
         <div className="">{props.icon}</div>
-        <div className="mt-3 mb-0">
-            <h4 className="m-0">{props.title}</h4>
-            <div className="line mb-3"></div>
-        </div>
+        <a className="mt-3 menu mb-0">
+          <h4 className="m-0 menu-title">{props.title}</h4>
+          <div className="line mb-3"></div>
+          <ul className="menu-dropdown">
+            {props.list.map((item) => {
+              return (
+                <li className="item" key={item}>
+                  {item}
+                </li>
+              );
+            })}
+          </ul>
+        </a>
       </div>
-
-      {props.list.map((item) => {
-        return (
-          <p className="item" key={item}>
-            {item}
-          </p>
-        );
-      })}
     </div>
   );
-}
+};
 
-export default Skills
+export default Skills;
